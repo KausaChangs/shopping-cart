1cf8261268a3598c3e1ad3f22d43a2381266dba5
KausaChangs <changayakausa@gmail.com>
2022-08-14 17:26:06 +0200

    added pricing to store cards

--- src/components/storeItem.tsx
+++ src/components/storeItem.tsx
@@ -16,6 +16,12 @@ export function StoreItem({ id, name, price, imgUrl }: StoreItemProps) {
         height="300px"
         style={{ objectFit: "cover" }}
       />
+      <Card.Body className="d-flex flex-column">
+        <Card.Title className="d-flex justify-content-between align-items-baseline mb-4">
+          <span className="fs-2">{name}</span>
+          <span className="ms-2 text-muted">{price}</span>
+        </Card.Title>
+      </Card.Body>
     </Card>
   );
 }
