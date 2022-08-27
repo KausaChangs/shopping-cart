da7893dc11694ae1e8f3d7422e6b6fa289ae0a7a
KausaChangs <changayakausa@gmail.com>
2022-08-27 09:45:52 +0200

    added a remove button to cards

--- src/components/storeItem.tsx
+++ src/components/storeItem.tsx
@@ -27,7 +27,28 @@ export function StoreItem({ id, name, price, imgUrl }: StoreItemProps) {
         <div className="mt-auto">
           {quantity === 0 ? (
             <Button className="w-100">Add to Cart</Button>
-          ) : null}
+          ) : (
+            <div
+              className="d-flex align-items-center flex-column"
+              style={{ gap: ".5rem" }}
+            >
+              <div
+                className="d-flex align-items-center  justify-content-center"
+                style={{ gap: ".5rem" }}
+              >
+                <Button>-</Button>
+                <div>
+                  <span className="fs-3">{quantity}</span>
+                  in cart
+                </div>
+                <Button>+</Button>
+              </div>
+              <Button variant="danger" size="sm">
+                {" "}
+                Remove{" "}
+              </Button>
+            </div>
+          )}
         </div>
       </Card.Body>
     </Card>
