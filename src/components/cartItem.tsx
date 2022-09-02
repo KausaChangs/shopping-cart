1c65ffeb84cc054379d5b7382220091e5955b73f
KausaChangs <changayakausa@gmail.com>
2022-09-02 17:11:05 +0200

    added first cart item description

--- src/components/cartItem.tsx
+++ src/components/cartItem.tsx
@@ -13,11 +13,21 @@ export function CartItem({ id, quantity }: CartItemProps) {
   if (item === null) return null;
 
   return (
-    <Stack direction="horizontal" gap={2}>
+    <Stack direction="horizontal" gap={2} className="d-flex align-items-center">
       <img
         src={item?.imgUrl}
         style={{ width: "125px", height: "75px", objectFit: "cover" }}
       />
+      <div className="me-auto">
+        <div>
+          {item.name}
+          {quantity > 1 && (
+            <span className="text-muted" style={{ fontSize: ".65rem" }}>
+              x{quantity}
+            </span>
+          )}
+        </div>
+      </div>
     </Stack>
   );
 }
