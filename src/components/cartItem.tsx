53e13a2bb13c2a10e236e8d73251cd34b970be3f
KausaChangs <changayakausa@gmail.com>
2023-08-15 08:48:29 +0200

    fixed problem causing undefined type checks

--- src/components/cartItem.tsx
+++ src/components/cartItem.tsx
@@ -11,7 +11,8 @@ type CartItemProps = {
 export function CartItem({ id, quantity }: CartItemProps) {
   const { removeFromCart } = useShoppingCart();
   const item = storeItems.find((i) => i.id === id);
-  if (item === null) return null;
+
+  if (!item) return null;
 
   return (
     <Stack direction="horizontal" gap={2} className="d-flex align-items-center">
